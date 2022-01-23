32bf21b6ffa083ad815bc9735d6f1eca941d829b
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-01-23 07:00:20 -0800

    Changes::For React Router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './mycomp/Navbar';
 import Textform from './mycomp/Textform';
-import About from './mycomp/About';
+// import About from './mycomp/About';
 import Alert from './mycomp/Alert';
 import React, { useState } from 'react'
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 
 function App() {
@@ -55,19 +55,22 @@ function App() {
     <>
     {/* <Navbar title={3} about="About" close="Disabled"/> */}
     {/* <Navbar> </Navbar> */}
-    <Router>
+    {/* <Router> */}
     <Alert alert={alert}></Alert>
     <Navbar title="React Store" about="About" close="Disabled" mode={mode} togglemode={togglemode} />
 
     <div className="container">
-    <Routes>
-      <Route path="/About" element= {<About />}>
-      </Route>
-    <Route path="/" element= {<Textform heading ="Enter The text to analysze" mode={mode} showalert={showalert}></Textform>}>
-    </Route>
-    </Routes>
+    {/* <Routes> */}
+      {/* <Route path="/About" element= {<About />}> */}
+      {/* </Route> */}
+    {/* <Route path="/" element= {<Textform heading ="Enter The text to analysze" mode={mode} showalert={showalert}></Textform>}> */}
+    <Textform heading ="Enter The text to analysze" mode={mode} showalert={showalert}>
+    </Textform>
+
+    {/* </Route> */}
+    {/* </Routes> */}
     </div>
-    </Router>
+    {/* </Router> */}
     
   </>
 
