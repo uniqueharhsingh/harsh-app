783776571629fdab7a8bf5a84ed8619ac7188e29
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-01-28 11:54:00 -0800

    changes unistall

--- src/mycomp/Navbar.js
+++ src/mycomp/Navbar.js
@@ -7,8 +7,8 @@ export default function Navbar(props) {
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
 
         <div className="container-fluid">
-          <link className="navbar-brand" to="/">{props.title}</link>
-          {/* <a className="navbar-brand" href="#">{props.title}</a> */}
+          {/* <Link className="navbar-brand" to="#">{props.title}</Link> */}
+          <a className="navbar-brand" href="/">{props.title}</a>
 
           
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,13 +17,13 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+                {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
                 
-              <Link className="nav-link" to="/About">{props.about}</Link>
-                {/* <a className="nav-link" href="#">{props.about}</a> */}
+              {/* <Link className="nav-link" to="/About">{props.about}</Link> */}
+                <a className="nav-link" href="/About">{props.about}</a>
 
                 
               </li>
